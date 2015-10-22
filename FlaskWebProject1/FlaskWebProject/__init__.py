176ae7f87c3d404f960fbbea85daf421a03fa1fb
Daniel Heinze <danielheinze@icloud.com>
2015-10-22 11:21:08 +0200

    Initial import of existing project.

---
+++ FlaskWebProject1/FlaskWebProject/__init__.py
@@ -0,0 +1,8 @@
+"""
+Initialize the flask website
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+import FlaskWebProject.views
---
+++ FlaskWebProject1/FlaskWebProject/views.py
@@ -0,0 +1,26 @@
+﻿"""
+Routes and views for the flask application.
+"""
+from datetime import datetime
+from flask import render_template, url_for, request
+from FlaskWebProject import app
+
+
+@app.route('/')
+@app.route('/home')
+def hello():
+    """Renders a sample page."""
+    return render_template(
+        'home.html',
+        title = 'Main Page',
+        year = datetime.now().year,
+        )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html'
+    )
+
+
